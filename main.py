ce4dbd236a35afe41b184b8364a0a59cea577ad3
Moyingo <moyam10@gmail.com>
2023-05-02 21:46:09 +0200

    Remove parser

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from src.eda import load_data, create_eda
 from src.preprocessing import transform_to_numeric
-import argparse
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        prog="fraud_detection",
-        description="Detect fraud",
-        epilog="",
-    )
-    parser.add_argument("-sv", "--sweetviz", default=False, type=bool)
-    return parser.parse_args()
-
-
-def main_train(arguments: dict):
+def main_train(sv: bool):
     """
     The main function is the entry point of the program.
     It calls all other functions in order to accomplish its task.
@@ -27,14 +16,11 @@ def main_train(arguments: dict):
     df = df.pipe(transform_to_numeric)
 
     # Create EDA
-    if arguments["sv"] == True:
+    if sv == True:
         create_eda(df=df)
 
     print(1)
 
 
 if __name__ == "__main__":
-    arguments = {}
-    parser = parse_args()
-    arguments["sv"] = parser.sweetviz
-    main_train(arguments=arguments)
+    main_train(sv=False)
